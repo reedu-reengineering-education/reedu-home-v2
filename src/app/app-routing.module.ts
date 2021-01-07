22149cd4de46ef8fad50b42cb2411767789f5228
felixerdy <felix.erdmann@uni-muenster.de>
2021-01-07 16:48:26 +0100

    add philosophie page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PhilosophieComponent } from './components/pages/about-us/philosophie/philosophie.component';
 import { AngeboteComponent } from './components/pages/angebote/angebote.component';
 import { PortfolioComponent } from './components/pages/portfolio/portfolio.component';
 import { StartComponent } from './components/pages/start/start.component';
@@ -7,7 +8,8 @@ import { StartComponent } from './components/pages/start/start.component';
 const routes: Routes = [
   { path: '', component: StartComponent },
   { path: 'angebote', component: AngeboteComponent },
-  { path: 'portfolio', component: PortfolioComponent }
+  { path: 'portfolio', component: PortfolioComponent },
+  { path: 'philosophie', component: PhilosophieComponent }
 ];
 
 @NgModule({
