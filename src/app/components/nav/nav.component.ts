d53a9359826b939babbc9b1bca63128e12c717ed
felixerdy <felix.erdmann@uni-muenster.de>
2021-01-08 16:39:43 +0100

    initial mobile navbar

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -11,21 +11,22 @@ export class NavComponent implements OnInit {
 
   currentLang = 'de';
   scroll = 0;
+  mobileOpen = false;
 
   constructor(private translate: TranslateService) { }
 
   ngOnInit(): void {
   }
 
-  selectLang(lang){
+  selectLang(lang) {
     this.currentLang = lang;
     this.translate.use(lang);
   }
 
   @HostListener('window:scroll', ['$event'])
   onScroll(event) {
-    this.scroll = window.pageYOffset 
-    || document.documentElement.scrollTop 
-    || document.body.scrollTop || 0; 
+    this.scroll = window.pageYOffset
+      || document.documentElement.scrollTop
+      || document.body.scrollTop || 0;
   }
 }
