48eb5dea8d266b30384e9b8aa1d6efa55273f0ed
felixerdy <felix.erdmann@uni-muenster.de>
2021-01-14 14:28:50 +0100

    seperate contact card component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,6 +18,7 @@ import { PrivacyComponent } from './components/pages/privacy/privacy.component';
 import { PhilosophieComponent } from './components/pages/about-us/philosophie/philosophie.component';
 import { AuszeichnungenComponent } from './components/pages/about-us/auszeichnungen/auszeichnungen.component';
 import { TeamComponent } from './components/pages/about-us/team/team.component';
+import { ContactCardComponent } from './components/contact-card/contact-card.component';
 
 export function createTranslateLoader(http: HttpClient) {
   return new TranslateHttpLoader(http, './assets/i18n/', '.json');
@@ -37,6 +38,7 @@ export function createTranslateLoader(http: HttpClient) {
     PhilosophieComponent,
     AuszeichnungenComponent,
     TeamComponent,
+    ContactCardComponent,
   ],
   imports: [
     BrowserModule,
