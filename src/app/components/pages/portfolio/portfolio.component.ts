01e5f2bbfc0032244fb6ad4673df66c9c1cae555
Umut Tas <u.tas@reedu.de>
2021-01-21 14:31:40 +0100

    filter modal

--- src/app/components/pages/portfolio/portfolio.component.ts
+++ src/app/components/pages/portfolio/portfolio.component.ts
@@ -11,18 +11,40 @@ import { TranslateService } from '@ngx-translate/core';
 export class PortfolioComponent implements OnInit {
 
   projects: any[];
+  customers;
+  angebote = ["Software", "Hardware", "Veranstaltungen", "Forschung", "Beratung"];
+  topics = [];
+  targets = [];
+  years = [2018, 2019, 2020]
+  filterOpen = false;
+
 
   constructor(private translate: TranslateService) { }
 
   ngOnInit(): void {
     this.projects = reProjectsDE;
-
+    this.buildFilters();
     this.translate.onLangChange.subscribe((event) => {
       if (event.lang === 'de') {
         this.projects = reProjectsDE;
       } else {
         this.projects = reProjectsEN;
       }
+      this.buildFilters();
     });
+  }
+
+  buildFilters() {
+    this.customers = [...new Set(this.projects.map(item => item.customer))]
+    this.projects.forEach(item => {
+      this.topics.push(...item.topics);
+      this.targets.push(...item.target)
+    })
+    this.topics = [...new Set(this.topics)]
+    this.targets = [...new Set(this.targets)]
+  }
+
+  toggleFilterModal() {
+    this.filterOpen = !this.filterOpen;
   }
 }
