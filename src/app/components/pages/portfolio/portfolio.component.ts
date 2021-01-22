69347b2f60133f4df78b35727dbb3cb6fa01f589
Umut Tas <u.tas@reedu.de>
2021-01-22 11:48:06 +0100

    filter logic

--- src/app/components/pages/portfolio/portfolio.component.ts
+++ src/app/components/pages/portfolio/portfolio.component.ts
@@ -11,7 +11,7 @@ import { TranslateService } from '@ngx-translate/core';
 export class PortfolioComponent implements OnInit {
 
   projects: any[];
-  filteredProject: any[];
+  filteredProjects: any[];
   customers;
   angebote = ["Software", "Hardware", "Veranstaltungen", "Forschung", "Beratung"];
   topics = [];
@@ -19,13 +19,27 @@ export class PortfolioComponent implements OnInit {
   years = [2018, 2019, 2020]
   filterOpen = false;
 
+  filter = {
+    customer: null,
+    topic: null,
+    angebot: null,
+    year: null,
+    target: null,
+  }
+
 
   constructor(private translate: TranslateService) { }
 
   ngOnInit(): void {
-    this.projects = reProjectsDE;
+    if(this.translate.currentLang === 'de' || this.translate.currentLang === undefined){
+      this.projects = reProjectsDE;
+    } else {
+      this.projects = reProjectsEN;
+    }
     this.buildFilters();
+    
     this.translate.onLangChange.subscribe((event) => {
+      console.log(this.translate.currentLang);
       if (event.lang === 'de') {
         this.projects = reProjectsDE;
       } else {
@@ -33,6 +47,7 @@ export class PortfolioComponent implements OnInit {
       }
       this.buildFilters();
     });
+    this.filteredProjects = this.projects;
   }
 
   buildFilters() {
@@ -45,7 +60,69 @@ export class PortfolioComponent implements OnInit {
     this.targets = [...new Set(this.targets)]
   }
 
+  changeCustomer(event){
+    if(event == 'null'){
+      this.filter.customer = null;
+    } else {
+      this.filter.customer = event;
+    }
+  }
+  changeTopic(event){
+    if(event == 'null'){
+      this.filter.topic = null;
+    } else {
+      this.filter.topic = event;
+    }
+  }
+  toggleAngebot(angebot){
+    if(this.filter.angebot != angebot){
+      this.filter.angebot = angebot;
+    } else {
+      this.filter.angebot = null;
+    }
+  }
+
+  toggleYear(year){
+    if(this.filter.year != year){
+      this.filter.year = year;
+    } else {
+      this.filter.year = null;
+    }
+  }
+
+  toggleTarget(target){
+    if(this.filter.target != target){
+      this.filter.target = target;
+    } else {
+      this.filter.target = null;
+    }
+  }
+
   toggleFilterModal() {
     this.filterOpen = !this.filterOpen;
+  }
+
+  applyFilters() {
+    this.filteredProjects = this.projects.filter(project => {
+      if(this.filter.customer && this.filter.customer != project.customer) {
+        return false;
+      }
+      if(this.filter.topic && project.topics.indexOf(this.filter.topic) === -1) {
+        return false;
+      }
+      if(this.filter.angebot && project.angebot.indexOf(this.filter.angebot) === -1) {
+        return false;
+      }
+      if(this.filter.year && project.year.indexOf(this.filter.year) === -1) {
+        return false;
+      }
+      if(this.filter.target && project.target.indexOf(this.filter.target) === -1 ) {
+        return false;
+      }
+      return true;
+
+      // if(this.filter.angebote && this.filter.angebote.indexOf())
+    })
+    this.filterOpen = false;
   }
 }
