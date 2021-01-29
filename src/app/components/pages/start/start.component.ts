d4489338872991cc3f8b1b0fc53d459a348c9429
Thiemann96 <e_thie10@uni-muenster.de>
2021-01-29 14:30:30 +0100

    working translations

--- src/app/components/pages/start/start.component.ts
+++ src/app/components/pages/start/start.component.ts
@@ -1,34 +1,71 @@
-import { Component, OnInit } from '@angular/core';
-import Typed  from 'typed.js';
+import { Component, enableProdMode, OnInit } from '@angular/core';
+import Typed from 'typed.js';
+import { TranslateService } from '@ngx-translate/core';
+const de = 
+[   "für Digitale Bildung",
+   "für Citizen Science",
+   "für Lernmaterialien",
+   "für Forschung",
+   "für GIS",
+   "für Smart Cities",
+   "für dich"]
+
+
+const en = [
+  "for Digital Education",
+   "for Citizen Science",
+   "for Educational Ressources",
+   "for Science",
+   "for GIS",
+   "for Smart Cities",
+   "for you"]
+
 
 @Component({
   selector: 're-start',
   templateUrl: './start.component.html',
   styleUrls: ['./start.component.scss']
 })
+
 export class StartComponent implements OnInit {
 
-  constructor() { }
+  constructor(private translate:TranslateService) { }
 
   ngOnInit(): void {
-    const options = {
-      strings: [
-      'für Digitale Bildung',
-      'für Citizen Science',
-      'für Lernmaterialien',
-      'für Forschung',
-      "für GIS",
-      "für Smart Cities",
-      "für dich/euch"
-      ],
+    let typedDe,typedEn;
+    let optionsEn = {
+      strings:en,
       typeSpeed: 100,
       backSpeed: 50,
       showCursor: false,
       cursorChar: '|',
       loop: true
- };
- 
- const typed = new Typed('.typed-element', options);
+    };
+    let optionsDe = {
+      strings:de,
+      typeSpeed: 100,
+      backSpeed: 50,
+      showCursor: false,
+      cursorChar: '|',
+      loop: true
+    };
+    if(this.translate.currentLang === 'de' || this.translate.currentLang === undefined){
+      typedDe = new Typed('.typed-element', optionsDe);
+    } else {
+      typedEn = new Typed('.typed-element', optionsEn);
+    }
+    this.translate.onLangChange.subscribe((event) => {
+      if (event.lang === 'de') {
+        typedEn.destroy();
+        typedDe = new Typed('.typed-element', optionsDe);
+      } else {
+        typedDe.destroy();
+        typedEn = new Typed('.typed-element', optionsEn);
+
+      }
+    });
+
+
   }
 
   onScrollClick() {
