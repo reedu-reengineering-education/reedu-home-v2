4d7d058b830cfff2f8534cc619c0e74480254e13
felixerdy <felix.erdmann@uni-muenster.de>
2021-01-29 15:09:37 +0100

    update auszeichnungen

--- src/app/components/pages/about-us/auszeichnungen/auszeichnungen.component.ts
+++ src/app/components/pages/about-us/auszeichnungen/auszeichnungen.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import awardsDe from '../../../../../assets/collections/awards.json';
+import awardsEn from '../../../../../assets/collections/awards-en.json';
+import { TranslateService } from '@ngx-translate/core';
 
 
 @Component({
@@ -12,10 +14,18 @@ export class AuszeichnungenComponent implements OnInit {
   awards: any[]
 
 
-  constructor() { }
+  constructor(public translate: TranslateService) { }
 
   ngOnInit(): void {
     this.awards = awardsDe;
+
+    this.translate.onLangChange.subscribe((event) => {
+      if (event.lang === 'de') {
+        this.awards = awardsDe;
+      } else {
+        this.awards = awardsEn;
+      }
+    });
   }
 
 }
