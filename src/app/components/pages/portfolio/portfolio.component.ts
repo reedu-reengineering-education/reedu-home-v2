5d325d3b4e270deb21314ebe88f571a3c3c5f9f0
Umut Tas <u.tas@reedu.de>
2021-02-12 10:47:50 +0100

    try wordcloud stuff

--- src/app/components/pages/portfolio/portfolio.component.ts
+++ src/app/components/pages/portfolio/portfolio.component.ts
@@ -54,6 +54,108 @@ export class PortfolioComponent implements OnInit {
 
   ngOnInit(): void {
 
+    function wordCloud(selector) {
+
+      var fill = d3.scaleOrdinal(d3Scale.schemeCategory10);
+  
+      //Construct the word cloud's SVG element
+      var svg = d3.select(selector).append("svg")
+          .attr("width", 500)
+          .attr("height", 500)
+          .append("g")
+          .attr("transform", "translate(250,250)");
+  
+  
+      //Draw the word cloud
+      function draw(words) {
+          var cloud = svg.selectAll("g text")
+                          .data(words, function(d) { return d.text; })
+  
+          //Entering words
+          cloud.enter()
+              .append("text")
+              .style("font-family", "Impact")
+              .style("fill", function(d, i) { return fill(i); })
+              .attr("text-anchor", "middle")
+              .attr('font-size', 1)
+              .text(function(d) { return d.text; });
+  
+          //Entering and existing words
+          cloud
+              .transition()
+                  .duration(600)
+                  .style("font-size", function(d) { return d.size + "px"; })
+                  .attr("transform", function(d) {
+                      return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
+                  })
+                  .style("fill-opacity", 1);
+  
+          //Exiting words
+          cloud.exit()
+              .transition()
+                  .duration(200)
+                  .style('fill-opacity', 1e-6)
+                  .attr('font-size', 1)
+                  .remove();
+      }
+  
+  
+      //Use the module pattern to encapsulate the visualisation code. We'll
+      // expose only the parts that need to be public.
+      return {
+  
+          //Recompute the word cloud for a new set of words. This method will
+          // asycnhronously call draw when the layout has been computed.
+          //The outside world will need to call this function, so make it part
+          // of the wordCloud return value.
+          update: function(words) {
+              cloud().size([500, 500])
+                  .words(words)
+                  .padding(5)
+                  .rotate(function() { return ~~(Math.random() * 2) * 90; })
+                  .font("Impact")
+                  .fontSize(function(d) { return d.size; })
+                  .on("end", draw)
+                  .start();
+          }
+      }
+  
+  }
+  
+  //Some sample data - http://en.wikiquote.org/wiki/Opening_lines
+  var words = [
+      "You don't know about me without you have read a book called The Adventures of Tom Sawyer but that ain't no matter.",
+      "The boy with fair hair lowered himself down the last few feet of rock and began to pick his way toward the lagoon.",
+      "When Mr. Bilbo Baggins of Bag End announced that he would shortly be celebrating his eleventy-first birthday with a party of special magnificence, there was much talk and excitement in Hobbiton.",
+      "It was inevitable: the scent of bitter almonds always reminded him of the fate of unrequited love."
+  ]
+  
+  //Prepare one of the sample sentences by removing punctuation,
+  // creating an array of words and computing a random size attribute.
+  function getWords(i) {
+      return words[i]
+              .replace(/[!\.,:;\?]/g, '')
+              .split(' ')
+              .map(function(d) {
+                  return {text: d, size: 10 + Math.random() * 10};
+              })
+  }
+  
+  //This method tells the word cloud to redraw with a new set of words.
+  //In reality the new words would probably come from a server request,
+  // user input or some other source.
+  function showNewWords(vis, i) {
+      i = i || 0;
+      console.log(words)
+      vis.update(getWords(i ++ % words.length))
+      setTimeout(function() { showNewWords(vis, i + 1)}, 2000)
+  }
+  
+  //Create a new instance of the word cloud visualisation.
+  var myWordCloud = wordCloud('body');
+  
+  //Start cycling through the demo data
+  showNewWords(myWordCloud,0);
     let that = this;
     
     this.route.queryParamMap.subscribe((queryParams:any) => {
@@ -101,10 +203,79 @@ export class PortfolioComponent implements OnInit {
 
   }
 
+  wordCloud(selector) {
+
+    var fill = d3.scaleOrdinal(d3Scale.schemeCategory10);
+
+    //Construct the word cloud's SVG element
+    var svg = d3.select(selector).append("svg")
+        .attr("width", 500)
+        .attr("height", 500)
+        .append("g")
+        .attr("transform", "translate(250,250)");
+
+
+    //Draw the word cloud
+    function draw(words) {
+        var cloud = svg.selectAll("g text")
+                        .data(words, function(d) { return d.text; })
+
+        //Entering words
+        cloud.enter()
+            .append("text")
+            .style("font-family", "Impact")
+            .style("fill", function(d, i) { return fill(i); })
+            .attr("text-anchor", "middle")
+            .attr('font-size', 1)
+            .text(function(d) { return d.text; });
+
+        //Entering and existing words
+        cloud
+            .transition()
+                .duration(600)
+                .style("font-size", function(d) { return d.size + "px"; })
+                .attr("transform", function(d) {
+                    return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
+                })
+                .style("fill-opacity", 1);
+
+        //Exiting words
+        cloud.exit()
+            .transition()
+                .duration(200)
+                .style('fill-opacity', 1e-6)
+                .attr('font-size', 1)
+                .remove();
+    }
+
+
+    //Use the module pattern to encapsulate the visualisation code. We'll
+    // expose only the parts that need to be public.
+    return {
+
+        //Recompute the word cloud for a new set of words. This method will
+        // asycnhronously call draw when the layout has been computed.
+        //The outside world will need to call this function, so make it part
+        // of the wordCloud return value.
+        update: function(words) {
+          cloud().size([500, 500])
+                .words(words)
+                .padding(5)
+                .rotate(function() { return ~~(Math.random() * 2) * 90; })
+                .font("Impact")
+                .fontSize(function(d) { return d.size; })
+                .on("end", draw)
+                .start();
+        }
+    }
+
+}
+
   drawWordCloud(words){
     words = words.flat()
     console.log(words);
-    
+    // console.log(d3Scale)
+    var fill = d3.scaleOrdinal(d3Scale.schemeCategory10);
     var countedWords = {};
     words.forEach(function(word){
       if(countedWords[word]){
@@ -122,37 +293,73 @@ export class PortfolioComponent implements OnInit {
 
     d3.select("#wordCloud").select('g').remove();
 
-    var layout = cloud()
-    .size([1000, 500])
-    .words(finalWords.map(function(d) {
-      console.log(Math.log(d.count))
-      return {text: d.text, size: Math.log(d.count+1)*20, test: "haha"};
-    }))
-    .padding(5)
-    // .rotate(function() { return ~~(Math.random() * 2) * 90; })
-    .rotate(function() { return 0; })
-    .font("Brixslab-Regular")
-    .fontSize(function(d) { return d.size; })
-    .on("end", draw);
+    // var layout = cloud()
+    // .size([1000, 500])
+    // .words(finalWords.map(function(d) {
+    //   console.log(Math.log(d.count))
+    //   return {text: d.text, size: Math.log(d.count+1)*20, test: "haha"};
+    // }))
+    // .padding(5)
+    // // .rotate(function() { return ~~(Math.random() * 2) * 90; })
+    // .rotate(function() { return 0; })
+    // .font("Brixslab-Regular")
+    // .fontSize(function(d) { return d.size; })
+    // .on("end", draw);
 
-    layout.start();
+    var svg = d3.select('#wordcloud-container').append("svg")
+    .attr("width", 1000)
+    .attr("height", 500)
+    .append("g")
+    .attr("transform", "translate(500,250)");
+
+    // layout.start();
 
     function draw(words) {
-      d3.select("#wordCloud")
-          .attr("width", layout.size()[0])
-          .attr("height", layout.size()[1])
-        .append("g")
-          .attr("transform", "translate(" + layout.size()[0] / 2 + "," + layout.size()[1] / 2 + ")")
-        .selectAll("text")
-          .data(words)
-        .enter().append("text")
-          .style("font-size", function(d) { return d.size + "px"; })
-          .style("font-family", "Brixslab-Regular")
-          .attr("text-anchor", "middle")
-          .attr("transform", function(d) {
-            return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
-          })
-          .text(function(d) { return d.text; });
+      var cloud = d3.select("#wordcloud-container").selectAll("g text")
+              .data(words, function(d) { return d.text; })
+              //Entering words
+              cloud.enter()
+              .append("text")
+              .style("font-family", "Impact")
+              .style("fill", function(d, i) { return fill(i); })
+              .attr("text-anchor", "middle")
+              .attr('font-size', 1)
+              .text(function(d) { return d.text; });
+  
+          //Entering and existing words
+          cloud
+              .transition()
+                  .duration(600)
+                  .style("font-size", function(d) { return d.size + "px"; })
+                  .attr("transform", function(d) {
+                      return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
+                  })
+                  .style("fill-opacity", 1);
+  
+          //Exiting words
+          cloud.exit()
+              .transition()
+                  .duration(200)
+                  .style('fill-opacity', 1e-6)
+                  .attr('font-size', 1)
+                  .remove();
+  
+
+      // d3.select("#wordCloud")
+      //     .attr("width", layout.size()[0])
+      //     .attr("height", layout.size()[1])
+      //   .append("g")
+      //     .attr("transform", "translate(" + layout.size()[0] / 2 + "," + layout.size()[1] / 2 + ")")
+      //   .selectAll("text")
+      //     .data(words)
+      //   .enter().append("text")
+      //     .style("font-size", function(d) { return d.size + "px"; })
+      //     .style("font-family", "Brixslab-Regular")
+      //     .attr("text-anchor", "middle")
+      //     .attr("transform", function(d) {
+      //       return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
+      //     })
+      //     .text(function(d) { return d.text; });
       }
 
   }
@@ -225,8 +432,8 @@ export class PortfolioComponent implements OnInit {
 
     })
     this.filterOpen = false;
-    this.drawWordCloud(this.filteredProjects.map(item => item.topics));
   }
+
 
   removeFilter(key){
     this.filter[key] = null;
