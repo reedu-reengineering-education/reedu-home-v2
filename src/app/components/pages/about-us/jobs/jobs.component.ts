722f3011cfdf4d7ef2a8f2931a195f4472c7996d
mostafa kandel <101183814+MostafaKandel@users.noreply.github.com>
2024-12-17 18:20:59 +0100

    feat: implementation a new design of jobs Page

--- src/app/components/pages/about-us/jobs/jobs.component.ts
+++ src/app/components/pages/about-us/jobs/jobs.component.ts
@@ -1,43 +1,9 @@
-// import { Component, OnInit } from '@angular/core';
-// import jobs from './../../../../../assets/collections/jobs/jobs.json'
-// import jobsEn from './../../../../../assets/collections/jobs/jobs-en.json'
-// import { TranslateService } from '@ngx-translate/core';
-
-// @Component({
-//   selector: 're-jobs',
-//   templateUrl: './jobs.component.html',
-//   styleUrls: ['./jobs.component.scss']
-// })
-// export class JobsComponent implements OnInit {
-
-//   jobs: any[]
-
-//   constructor(private translate: TranslateService) { }
-
-//   ngOnInit(): void {
-//     this.jobs = jobs
-
-//     if(this.translate.currentLang === 'de' || this.translate.currentLang === undefined){
-//       this.jobs = jobs
-//     } else {
-//       this.jobs = jobsEn;
-//     }
-
-//     this.translate.onLangChange.subscribe((event) => {
-//       if (event.lang === 'de') {
-//         this.jobs = jobs
-//       } else {
-//         this.jobs = jobsEn;
-//       }
-//     });
-//   }
-
-// }
+ 
 import { Component, OnInit } from '@angular/core';
 import jobs from './../../../../../assets/collections/jobs/jobs.json';
 import jobsEn from './../../../../../assets/collections/jobs/jobs-en.json';
-import benefits from './../../../../../assets/collections/benefits/benefits-de.json';
-import benefitsEn from './../../../../../assets/collections/benefits/benefits-eng.json';
+// import benefits from './../../../../../assets/collections/benefits/benefits-de.json';
+// import benefitsEn from './../../../../../assets/collections/benefits/benefits-eng.json';
 import jobsHead from './../../../../../assets/collections/jobs/jobs-head.json';
 import jobsHeadEn from './../../../../../assets/collections/jobs/jobs-head-en.json';
 
@@ -51,7 +17,7 @@ import { TranslateService } from '@ngx-translate/core';
 export class JobsComponent implements OnInit {
 
   jobs: any[] = [];
-  benefits: any[] = [];
+  // benefits: any[] = [];
   jobsHead: any = {};
 
   constructor(private translate: TranslateService) { }
@@ -69,12 +35,12 @@ export class JobsComponent implements OnInit {
   loadData(): void {
     // Load jobs, benefits, and job head based on the current language
     if (this.translate.currentLang === 'de' || this.translate.currentLang === undefined) {
-      this.jobs = jobs;  // Load German jobs
-      this.benefits = benefits;  // Load German benefits
-      this.jobsHead = jobsHead;  // Load German job head
+      this.jobs = jobs;  
+      // this.benefits = benefits;  
+      this.jobsHead = jobsHead;  
     } else {
       this.jobs = jobsEn;  // Load English jobs
-      this.benefits = benefitsEn;  // Load English benefits
+      // this.benefits = benefitsEn;  // Load English benefits
       this.jobsHead = jobsHeadEn;  // Load English job head
     }
   }
